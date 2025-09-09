152d469c084512758cf285020a26c0351f32aae2
Mikael Larsson <mikael.j.larsson@hotmail.com>
2025-09-09 12:00:30 +0200

    refactor: remove unused Radix UI drawer component from App

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,6 @@ import React from "react";
 import Header from "./components/Header";
 import ProductImage from "./components/ProductImage";
 import ProductInfo from "./components/ProductInfo";
-import {
-  Drawer,
-  DrawerContent,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerClose,
-} from "@/components/ui/drawer";
 
 function App() {
   return (
@@ -20,35 +13,6 @@ function App() {
           <ProductInfo />
         </main>
       </div>
-      <Drawer>
-        <DrawerContent>
-          <DrawerHeader>Main menu</DrawerHeader>
-          <nav className="main-menu">
-            <ul>
-              <li>
-                <a href="#">Collections</a>
-              </li>
-              <li>
-                <a href="#">Men</a>
-              </li>
-              <li>
-                <a href="#">Women</a>
-              </li>
-              <li>
-                <a href="#">About</a>
-              </li>
-              <li>
-                <a href="#">Contact</a>
-              </li>
-            </ul>
-          </nav>
-          <DrawerFooter>
-            <DrawerClose asChild>
-              <button type="button">Close</button>
-            </DrawerClose>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
     </div>
   );
 }
