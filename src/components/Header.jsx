ba6360eefad2f58f5c2dbc1331576085ba8c9869
Mikael Larsson <mikael.j.larsson@hotmail.com>
2025-09-09 12:24:25 +0200

    feat: implement sidebar functionality and update styles for improved layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,66 @@
-import React from 'react';
+import React, { useState } from "react";
+import Sidebar from "@/components/Sidebar";
 
 const Header = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const handleMenuClick = (ev) => {
+    ev.preventDefault();
+    setIsSidebarOpen(isSidebarOpen ? false : true);
+  };
   return (
     <header>
       <button
         className="hamburger-menu md:hidden"
         type="button"
         aria-label="Menu"
-      ></button>
-      <a href="#" className="logo">
-        <img src="/images/logo.svg" alt="Sneaker Company logo" />
+        onClick={(ev) => handleMenuClick(ev)}
+      />
+      <a
+        href="#"
+        className="logo"
+      >
+        <img
+          src="/images/logo.svg"
+          alt="Sneaker Company logo"
+        />
       </a>
       <nav className="main-navigation hidden md:grid">
         <ul>
-          <li><a href="#">Collections</a></li>
-          <li><a href="#">Men</a></li>
-          <li><a href="#">Women</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
+          <li>
+            <a href="#">Collections</a>
+          </li>
+          <li>
+            <a href="#">Men</a>
+          </li>
+          <li>
+            <a href="#">Women</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
         </ul>
       </nav>
-      <button className="cart-button" type="button">
-        <img src="/images/icon-cart.svg" alt="Shopping cart icon" />
+      <button
+        className="cart-button"
+        type="button"
+      >
+        <img
+          src="/images/icon-cart.svg"
+          alt="Shopping cart icon"
+        />
       </button>
       <button
         type="button"
         className="user-avatar"
         aria-label="User avatar"
       ></button>
+      <Sidebar
+        open={isSidebarOpen}
+        handleClose={handleMenuClick}
+      />
     </header>
   );
 };
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,41 @@
-import React from 'react';
+import clsx from "clsx";
+import React from "react";
 
-const Sidebar = () => {
+const Sidebar = ({ open, handleClose }) => {
   return (
     <>
-      <div className="sidepanel sm:block">
-        <button className="close" aria-label="Close panel">
+      <div className={clsx("sidepanel sm:block", open ? "open" : "")}>
+        <button
+          className="close"
+          aria-label="Close panel"
+          onClick={(ev) => handleClose(ev)}
+        >
           X
         </button>
         <nav className="main-menu">
           <ul>
-            <li><a href="#">Collections</a></li>
-            <li><a href="#">Men</a></li>
-            <li><a href="#">Women</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
+            <li>
+              <a href="#">Collections</a>
+            </li>
+            <li>
+              <a href="#">Men</a>
+            </li>
+            <li>
+              <a href="#">Women</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
           </ul>
         </nav>
       </div>
-      <div className="tint"></div>
+      <div
+        className={clsx("tint", !open ? "hidden" : "")}
+        onClick={(ev) => handleClose(ev)}
+      />
     </>
   );
 };
